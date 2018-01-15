92390e1fc292a2ef5fada50933a40f43ed63e1fc
cpretzer <charles@nginx.com>
2018-01-15 09:21:18 -0800

    * MRADEV-713 prepare for release

---
+++ README.md
@@ -0,0 +1,149 @@
+# NGINX Microservices Reference Architecture: User Manager Service
+This repository contains a Python application which is used to store and modify user information for the NGINX 
+_Ingenious_ application. The _Ingenious_ application has been developed by the NGINX Professional Services team to provide a 
+reference  architecture for building your own microservices based application using NGINX as the service mesh. 
+
+The default configuration for all the components of the MRA, including the User Manager service, is to use the 
+[Fabric Model Architecture](https://www.nginx.com/blog/microservices-reference-architecture-nginx-fabric-model/ "Fabric Model").
+Instructions for using the [Router Mesh](https://www.nginx.com/blog/microservices-reference-architecture-nginx-router-mesh-model/) or 
+[Proxy Model](https://www.nginx.com/blog/microservices-reference-architecture-nginx-proxy-model/) architectures will be made available in the future.
+
+## Quick start
+As a single service in the set of services which comprise the NGINX Microservices Reference Architecture application, _Ingenious_,
+the User Manager service is not meant to function as a standalone service. Once you have built the image, it can be deployed 
+to a container engine along with the other components of the _Ingenious_ application, and then the application will be 
+accessible via your browser. 
+
+There are detailed instructions for building the service below, and in order to get started quickly, you can follow these simple 
+instructions to quickly build the image.
+
+0. (Optional) If you don't already have an NGINX Plus license, you can request a temporary developer license 
+[here](https://www.nginx.com/developer-license/ "Developer License Form"). If you do have a license, then skip to the next step. 
+1. Copy your licenses to the **<repository-path>/mra-user-manager/nginx/ssl** directory
+2. Run the command `docker build . -t <your-image-repo-name>/user-manager:quickstart` where <image-repository-name> is the username
+for where you store your Docker images
+3. Once the image has been built, push it to your image repository with the command `docker push -t <your-image-repo-name>/user-manager:quickstart`
+
+At this point, you will have an image that is suitable for deployment on to a Kubernetes installation, and you can deploy the
+image by creating YAML files and uploading them to your Kubernetes installation.
+
+To build a customized image for different container engines or to set other options, please follow the directions below.
+
+## Building a Customized Docker Image
+The [Dockerfile](Dockerfile) for the User Manager service is based on the [python:3.5](https://github.com/docker-library/python/blob/6ebbaa8a56cdf4021c78e87b3872be3861ac072a/3.5/jessie/Dockerfile) image, 
+and installs NGINX open source or NGINX Plus. Note that NGINX Plus includes features which make discovery of other services possible, include additional load balancing algorithms, 
+create persistent SSL/TLS connections, and provide advanced health check functionality.
+
+Please refer to the comments in the [Dockerfile](Dockerfile) for details about each command which is
+used to build the image. 
+
+The command, or entrypoint, for the [Dockerfile](Dockerfile) is the [start.sh script](app/start.sh "Dockerfile entrypoint"). 
+This script sets some local variables, then runs [uwsgi](https://uwsgi-docs.readthedocs.io/en/latest/ "USWGI") 
+to execute the logic in the [app.py](app/app.py) file. It also starts NGINX to send requests to the port opened by UWSGI.
+The configuration for UWSGI is found in [uwsgi.ini](app/uwsgi.ini)
+
+### 1. Build options
+The [Dockerfile](Dockerfile) sets some ENV arguments which are used when the image is built:
+
+- **USE_NGINX_PLUS**  
+    The default value is true. When this value is set to false, NGINX open source will be built in to the image and several 
+    features, including service discovery and advanced load balancing will be disabled.
+    See [installing nginx plus](#installing-nginx-plus)
+    
+- **USE_VAULT**  
+    The default value is false. Setting this value to true will cause install-nginx.sh to look 
+    for a file named vault_env.sh which contains the _VAULT_ADDR_ and _VAULT_TOKEN_ environment variables to
+    retrieve NGINX Plus keys from a [vault](https://www.vaultproject.io/) server.
+    
+    ```
+    #!/bin/bash
+    export VAULT_ADDR=<your-vault-address>
+    export VAULT_TOKEN=<your-vault-token>
+    ```
+    
+    You must be certain to include the vault_env.sh file when _USE_VAULT_ is true. There is an entry in the [.gitignore](.gitignore)
+    file for vault_env.sh
+    
+    In the future, we will release an article on our [blog](https://www.nginx.com/blog/) describing how to use vault with NGINX.    
+    
+- **CONTAINER_ENGINE**  
+    The container engine used to run the images in a container. _CONTAINER_ENGINE_ can be one of the following values
+     - kubernetes (default): to run on Kubernetes
+        When the nginx.conf file is built, the [fabric_config_k8s.yaml](nginx/fabric_config_k8s.yaml) will be
+        used to populate the open source version of the [nginx.conf template](nginx/nginx-plus-fabric.conf.j2)
+     - mesos: to run on DC/OS
+        When the nginx.conf file is built, the [fabric_config.yaml](nginx/fabric_config.yaml) will be
+        used to populate the open source version of the [nginx.conf template](nginx/nginx-plus-fabric.conf.j2)  
+     - local: to run in containers on the machine where the repository has been cloned
+        When the nginx.conf file is built, the [fabric_config_local.yaml](nginx/fabric_config_local.yaml) will be
+        used to populate the open source version of the [nginx.conf template](nginx/nginx-plus-fabric.conf.j2)                  
+     
+### 2. Decide whether to use NGINX Open Source or NGINX Plus
+#### <a href="#" id="installing-nginx-oss"></a>Installing NGINX Open Source
+Set the _USE_NGINX_PLUS_ property to false in the [Dockerfile](Dockerfile)
+#### <a href="#" id="installing-nginx-plus"></a>Installing NGINX Plus
+Before installing NGINX Plus, you'll need to obtain your license keys. If you do not already have a valid NGINX Plus subscription, you can request 
+developer licenses [here](https://www.nginx.com/developer-license/ "Developer License Form") 
+
+Set the _USE_NGINX_PLUS_ property to true in the [Dockerfile](Dockerfile)
+
+By default _USE_VAULT_ is set to false, and you must manually copy your **nginx-repo.crt** and **nginx-repo.key** 
+files to the **<path-to-repository>/mra-user-manager/nginx/ssl/** directory.
+
+Download the **nginx-repo.crt** and **nginx-repo.key** files for your NGINX Plus Developer License or subscription, and move them to the root directory of this project. You can then copy both of these files to the **/nginx/ssl** directory of each microservice using the command below:
+```
+cp nginx-repo.crt nginx-repo.key <repository>/nginx/ssl/
+```
+If _USE_VAULT_ is set to true, you must have installed a vault server and written the contents of the **nginx-repo.crt**
+and **nginx-repo.key** file to vault server. Refer to the vault documentation for instructions configuring a vault server
+and adding values to it. 
+
+### 3. Decide which container engine to use
+#### Set the _CONTAINER_ENGINE_ variable
+As described above, the _CONTAINER_ENGINE_ environment variable must be set to one of the following three options.
+The [install-nginx.sh](install-nginx.sh) file uses this value to determine which template file to use when populating the nginx.conf file.
+- kubernetes 
+- mesos 
+- local
+
+### 4. Build the image
+Replace _&lt;your-image-repo-name&gt;_ with the username for where you store your Docker images, and execute the command below to build the image. The _&lt;tag&gt;_ argument is optional and defaults to **latest**
+```
+docker build . -t <your-image-repo-name>/user-manager:<tag>
+```
+
+### 5. Runtime environment variables
+In order to run the image, some environment variables must be set so that they are available during runtime.
+
+| Variable Name         | Description                                                                                       | Example Value                 |
+| --------------------- | ------------------------------------------------------------------------------------------------- | ----------------------------- |
+| ALBUM_MANAGER_URL     |                                                                                                   | http://localhost/album-manager|
+| AWS_ACCESS_KEY_ID     | Your AWS Key for S3                                                                               | ABCD1234ABCD1234ABCD1234      |
+| AWS_REGION            | The region where your S3 instance is running                                                      | us-west-1                     |
+| AWS_SECRET_ACCESS_KEY | Your AWS Secret Access Key                                                                        | ABCD1234ABCD1234ABCD1234      |
+| DB_ENDPOINT           | The host of the DynamoDB instance                                                                 | http://dynamo-db:8000         |
+| DEV_MODE              | (Optional) For development only, monitor python modules to trigger reload                         | false                         |
+| VERIFY_CERTS          | (Optional) For development, set an environment variable to disable HTTPS certificate verification | false                         |
+
+### 6. Service Endpoints
+
+| Method | Endpoint             | Description             | Parameters              |
+| ------ | -------------------- | ----------------------- | ----------------------- |
+| GET    | /                    | Succesful operation     |                         |
+| POST   | /users               | Creates user            | body - user information |
+| GET    | /users/facebook/{id} | Get user by Facebook id | id - id for user        |
+| GET    | /users/google/{id}   | Get user by Google id   | id - id for user        |
+| GET    | /users/local/{id}    | Get user by local id    | id - id for user        |
+| GET    | /users/email/{email} | Get user by email       | email - email of user   |
+| POST   | /users/email/auth    | Authenticate user       | body - user information |
+| GET    | /users/{id}          | Get user by user id     | id - id for user        |
+| PUT    | /users/{id}          | Update user by user id  | id - id for user        |
+| DELETE | /users/{id}          | Delete user by user id  | id - id for user        |
+
+### Disclaimer
+In this service, the **nginx/ssl/dhparam.pem** file is provided for ease of setup. In production environments, it is highly recommended for secure key-exchange to replace this file with your own generated DH parameter.
+
+You can generate your own **dhparam.pem** file using the command below:
+```
+openssl dhparam -out nginx/ssl/dhparam.pem 2048
+```
---
+++ app/app.py
@@ -0,0 +1,379 @@
+#!/usr/bin/env python3
+
+import boto3
+import botocore
+import connexion
+import logging
+import uuid
+import os
+import requests
+
+from flask import abort, g
+
+from boto3.dynamodb.conditions import Key
+
+from os.path import join, dirname
+from dotenv import load_dotenv
+from werkzeug.security import generate_password_hash, check_password_hash
+
+#
+#  app.py
+#  UserManager
+#
+#  Copyright © 2017 NGINX Inc. All rights reserved.
+#
+
+#
+# The User Manager service supports .env files. You can create a file named .env in the app directory
+# in order to set environment variables for the app
+#
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+#
+# Get the DB address as a string from an environment variable
+#
+db_endpoint = os.environ.get('DB_ENDPOINT')
+
+#
+# For development, set an environment variable to disable HTTPS certificate verification
+#
+verify_certs = os.environ.get('VERIFY_CERTS') != 'false'
+
+
+#
+# Defines the healthcheck endpoint to ensure that the service is running
+#
+def healthcheck():
+    return '', 204
+
+
+#
+# Get the database based on the db_endpoint variable set from the DB_ENDPOINT environment variable
+#
+def get_db():
+    if __name__ == '__main__':
+        db = getattr(g, '_database', None)
+        if db is None:
+            db = g._database = boto3.resource('dynamodb', region_name='us-west-1', endpoint_url=db_endpoint)
+    else:
+        db = boto3.resource('dynamodb', region_name='us-west-1', endpoint_url=db_endpoint)
+    return db
+
+
+#
+# Get the users table from the database
+# TODO: This is a hack. We will clean this up in v2. For the purposes of a
+# reference application, we check for the able and create it if it doesn't
+# exist. Production grade services should remove this logic in favor of
+# a proper script to create the table
+#
+def get_users_table():
+
+    client = get_db()
+    try:
+        table = client.create_table(
+            TableName='users',
+            KeySchema=[
+                {
+                    'AttributeName': 'id',
+                    'KeyType': 'HASH'
+                }
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'id',
+                    'AttributeType': 'S'
+                },
+                {
+                    'AttributeName': 'google_id',
+                    'AttributeType': 'S'
+                },
+                {
+                    'AttributeName': 'facebook_id',
+                    'AttributeType': 'S'
+                },
+                {
+                    'AttributeName': 'local_id',
+                    'AttributeType': 'S'
+                },
+                {
+                    'AttributeName': 'email',
+                    'AttributeType': 'S'
+                }
+            ],
+            GlobalSecondaryIndexes=[
+                {
+                    'IndexName': 'google_id-index',
+                    'KeySchema': [
+                        {
+                            'AttributeName': 'google_id',
+                            'KeyType': 'HASH'
+                        },
+                    ],
+                    'Projection': {
+                        'ProjectionType': 'ALL',
+                    },
+                    'ProvisionedThroughput': {
+                        'ReadCapacityUnits': 5,
+                        'WriteCapacityUnits': 5
+                    }
+                },
+                {
+                    'IndexName': 'facebook_id-index',
+                    'KeySchema': [
+                        {
+                            'AttributeName': 'facebook_id',
+                            'KeyType': 'HASH'
+                        },
+                    ],
+                    'Projection': {
+                        'ProjectionType': 'ALL',
+                    },
+                    'ProvisionedThroughput': {
+                        'ReadCapacityUnits': 5,
+                        'WriteCapacityUnits': 5
+                    }
+                },
+                {
+                    'IndexName': 'email_address-index',
+                    'KeySchema': [
+                        {
+                            'AttributeName': 'email',
+                            'KeyType': 'HASH'
+                        },
+                    ],
+                    'Projection': {
+                        'ProjectionType': 'ALL',
+                    },
+                    'ProvisionedThroughput': {
+                        'ReadCapacityUnits': 5,
+                        'WriteCapacityUnits': 5
+                    }
+                },
+                {
+                    'IndexName': 'local_id-index',
+                    'KeySchema': [
+                        {
+                            'AttributeName': 'local_id',
+                            'KeyType': 'HASH'
+                        },
+                    ],
+                    'Projection': {
+                        'ProjectionType': 'ALL',
+                    },
+                    'ProvisionedThroughput': {
+                        'ReadCapacityUnits': 5,
+                        'WriteCapacityUnits': 5
+                    }
+                }
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 5,
+                'WriteCapacityUnits': 5
+            }
+        )
+
+        logging.debug("+++++++++ table: " + str(table))
+
+        if table is not None and len(table) > 0:
+            table.meta.client.get_waiter('table_exists').wait(TableName='users')
+            
+    except Exception as e:
+        logging.error('===== error is', e)
+
+        if e is AttributeError:
+            logging.error('++++++ attribute error', e);
+
+        if e is botocore.exceptions.ClientError and 'Error' in e.response and \
+                e.response['Error']['Code'] == 'ResourceInUseException':
+            logging.debug('======= looking for users table: already exists')
+        else:
+            logging.error('Unable to process error', e)
+
+    return client.Table('users')
+
+
+#
+# Get the user with the key and ID from the index
+# @param index: the index to use when searching for the user it should be one of:
+#        - email_address-index
+#        - facebook_id-index
+#        - google_id-index
+#        - local_id-index
+# @param key: the name of the key to use when searching the index
+# @param id: the ID of the item to find
+#
+def get_user_by_index_and_key(index, key, id):
+    user_response = get_users_table().query(
+        IndexName=index,
+        KeyConditionExpression=Key(key).eq(id)
+    )
+
+    if not user_response['Items']:
+        return None
+
+    item = user_response['Items'][0]
+    return item
+
+
+#
+# Create a user with the values provided in the in the body parameter. Also
+#          creates default albums for profile pictures and cover pictures
+#
+# @param body: contains the values for creating a user:
+#         - auth_id
+#         - auth_provider
+#         - email_address
+#
+def create_user(body) -> str:
+    body['id'] = str(uuid.uuid4())
+
+    # set the password and local_id if the body dictionary contains an entry named password
+    if 'password' in body:
+        body['local_id'] = str(uuid.uuid4())
+        body['password'] = generate_password_hash(body['password'])
+
+    # insert the user record in to the table
+    get_users_table().put_item(Item=body)
+
+    url = os.environ.get('ALBUM_MANAGER_URL')
+    headers = {'Auth-ID': body['id']}
+
+    # Call the album manager to create the Profile Pictures album
+    data = {'album[name]': 'Profile Pictures', 'album[state]': 'active'}
+    r = requests.post(url, headers=headers, data=data, verify=verify_certs)
+    pp_r = r.json()
+
+    # Call the album manager to create the Cover Pictures album
+    data = {'album[name]': 'Cover Pictures', 'album[state]': 'active'}
+    r = requests.post(url, headers=headers, data=data, verify=verify_certs)
+    cv_r = r.json()
+
+    # set parameters in the body dictionary and update the user entry in the
+    # user table
+    body_albums_ids = {'profile_pictures_id': str(pp_r['id']), 'cover_pictures_id': str(cv_r['id']),
+                       'profile_picture_url': 'generic'}
+    body = update_user(body['id'], body_albums_ids)
+
+    return body
+
+
+#
+# Find the user by the ID generated in create_user
+# @param id: the ID of the user to find
+#
+def get_user_by_id(id) -> str:
+    response = get_users_table().get_item(Key={'id': id})
+
+    if 'Item' not in response:
+        abort(404)
+
+    return response['Item']
+
+
+#
+# Find the user by the facebook ID
+# @param id: the facebook_id of the user to find
+#
+def get_user_by_facebook_id(id) -> str:
+    result = get_user_by_index_and_key('facebook_id-index', 'facebook_id', id)
+
+    if result is None:
+        abort(404)
+
+    return result
+
+
+#
+# Find the user by the Google ID
+# @param id: the google_id of the user to find
+#
+def get_user_by_google_id(id) -> str:
+    result = get_user_by_index_and_key('google_id-index', 'google_id', id)
+
+    if result is None:
+        abort(404)
+
+    return result
+
+
+#
+# Find a user by the local_id
+# @param id: the local ID of the user to find
+#
+def get_user_by_local_id(id) -> str:
+    result = get_user_by_index_and_key('local_id-index', 'local_id', id)
+
+    if result is None:
+        abort(404)
+
+    return result
+
+
+#
+# Find a user by their email address
+# @param email: the email address of the user to find
+#
+def get_user_by_email(email) -> str:
+    result = get_user_by_index_and_key('email_address-index', 'email', email)
+
+    if result is None:
+        result = {'found': False}
+
+    return result
+
+
+#
+# Authenticate a local user by comparing the password hash using methods in
+#           the werkzeug library
+#
+# @param body: the POST request body
+#
+def auth_local_user(body) -> str:
+    email = body['email']
+    password = body['password']
+
+    if email and password:
+        user = get_user_by_email(email)
+        if user:
+            body['authenticated'] = check_password_hash(user['password'], password)
+
+    return body
+
+
+#
+# Update the user with the data provided
+#
+# @param id: the ID of the user to update
+# @param body: the post data to use when updating the user
+#
+def update_user(id, body) -> str:
+    item = get_user_by_id(id)
+
+    for key, value in body.items():
+        item[key] = value
+
+    get_users_table().put_item(Item=item)
+    return item
+
+
+#
+# Permanently deletes a user by their ID
+# @param id: the ID of the user to delete
+#
+def delete_user(id) -> str:
+    if get_user_by_id(id):
+        return get_users_table().delete_item(Key={'id': id})
+
+
+#
+# Initialize the application, logging, and swagger definition
+#
+logging.basicConfig(level=logging.DEBUG)
+app = connexion.App(__name__)
+app.add_api('swagger.yaml')
+application = app.app
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
