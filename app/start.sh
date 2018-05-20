221087538d8ff530c100ec5e9228379b0e0d46e3
Eduard Borges <eduardborges@me.com>
2018-05-20 08:50:18 +0200

    MRADEV-911_MRADEV-912_MRADEV-913

--- app/start.sh
+++ app/start.sh
@@ -4,31 +4,38 @@ NGINX_CONF=""
 APP="uwsgi --ini uwsgi.ini"
 DEBUG=""
 
-case "$NETWORK" in
-    fabric)
-        NGINX_CONF="/etc/nginx/fabric_nginx_$CONTAINER_ENGINE.conf"
-        echo 'Fabric configuration set'
-        nginx -c "$NGINX_CONF" -g "pid $NGINX_PID;" &
-        ;;
-    router-mesh)
-        ;;
-    *)
-        echo 'Network not supported'
-esac
-
 if [ "$DEV_MODE" = "true" ]
 then
     DEBUG=" --py-autoreload 5"
 fi
 
 # start the application using UWSGI
-$APP $DEBUG
+${APP} ${DEBUG} &
 
 sleep 30
-APP_PID=`ps aux | grep $APP | grep -v grep`
+APP_PID=`ps aux | grep ${APP} | grep -v grep`
 
-while [ -f "$NGINX_PID" ] &&  [ "$APP_PID" ];
-do 
-	sleep 5;
-	APP_PID=`ps aux | grep $APP | grep -v grep`;
-done
+case "$NETWORK" in
+    fabric)
+        NGINX_CONF="/etc/nginx/fabric_nginx_$CONTAINER_ENGINE.conf"
+        echo 'Fabric configuration set'
+        nginx -c "$NGINX_CONF" -g "pid $NGINX_PID;" &
+
+        sleep 30
+        while [ -f "$NGINX_PID" ] &&  [ "$APP_PID" ];
+        do
+	        sleep 5;
+	        APP_PID=`ps aux | grep ${APP} | grep -v grep`;
+        done
+        ;;
+    router-mesh)
+        while [ "$APP_PID" ];
+        do
+	        sleep 5;
+	        APP_PID=`ps aux | grep ${APP} | grep -v grep`;
+        done
+        ;;
+    *)
+        echo 'Network not supported'
+        exit 1
+esac
